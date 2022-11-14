25fc6f2f34f0726915d7aa8be027684875696695
winpc <roland.felt@outlook.com>
2022-11-14 18:53:48 +0100

    #3 Het werkt maar ik krijg wel exceptions op de achtergrond

--- src/main/java/nu/educom/MI6/Login.java
+++ src/main/java/nu/educom/MI6/Login.java
@@ -2,11 +2,10 @@ package nu.educom.MI6;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class Login {
     String codeSentence = "For ThE Royal QUEEN";
-    int serviceNumber = 0;
-    String actualNumber = "";
     ArrayList<Integer> blacklist;
     ArrayList<Integer> loggedIn;
 
@@ -16,46 +15,52 @@ public class Login {
     }
 
     public void checkLogin() {
-        System.out.println("Enter service number");
+        JFrame frame = new JFrame("InputDialog Example #1");
         boolean needCodeSentence = true;
-        while (true) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                int userInput = scanner.nextInt();
+        int serviceNumber = 0;
+        String actualNumber = "";
 
+        while (true) {
+            String jInput = JOptionPane.showInputDialog(frame, "Enter service number:");
+            // If user clicks cancel, system quits.
+            if (jInput == null) {System.exit(0);}
+
+            // Otherwise checks the value
+            try {
+                int userInput = Integer.parseInt(jInput);
+                serviceNumber = userInput;
+                actualNumber = String.format("%03d", serviceNumber);
                 if (this.blacklist.contains(userInput)) {
-                    System.out.println("You are still a failure " + String.format("%03d", serviceNumber));
+                    JOptionPane.showMessageDialog(null, String.format("You are still a failure %s", actualNumber));
                     needCodeSentence = false;
                     break;
                 } else if (this.loggedIn.contains(userInput)) {
-                    System.out.println("You have already logged in " + String.format("%03d", serviceNumber));
+                    JOptionPane.showMessageDialog(null, String.format("You have already logged in %s", actualNumber));
                     needCodeSentence = false;
                     break;
                 } else if (0 < userInput && userInput < 957) {
-                    serviceNumber = userInput;
-                    actualNumber = String.format("%03d", serviceNumber);
                     break;
                 }
-                System.out.println("Which is false, please enter a number between 1-956");
+                JOptionPane.showMessageDialog(null, "Which is false, please enter a number between 1-956");
             } catch (Exception e) {
-                System.out.println("Which is false, please enter a number");
+                JOptionPane.showMessageDialog(null,"Which is false, please enter a number");
             }
         }
 
-        // VRAAG: Kan ik hier met een %s ook inserten?
         if (needCodeSentence) {
-            System.out.println("Please enter code-sentence " + actualNumber);
-            Scanner scanner = new Scanner(System.in);
-            String userInput = scanner.nextLine();
-
+            String jInput = JOptionPane.showInputDialog(frame,  String.format("Please enter code-sentence %s", actualNumber));
+            // If user clicks cancel, system quits.
+            if (jInput == null) {System.exit(0);}
+            String userInput = jInput;
             if (userInput.equals(codeSentence)) {
-                System.out.println("You have logged in successfully " + actualNumber);
+                JOptionPane.showMessageDialog(null, String.format("You have logged in successfully  %s", actualNumber));
                 loggedIn.add(serviceNumber);
 
             } else {
-                System.out.println("You are now blacklisted " + actualNumber);
+                JOptionPane.showMessageDialog(null, String.format("ACCESS DENIED  %s", actualNumber));
                 blacklist.add(serviceNumber);
             }
         }
     }
+
 }

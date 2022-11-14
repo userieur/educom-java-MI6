d8cbf876f7855f9ad4509d30c4b64f69a5e2a08e
winpc <roland.felt@outlook.com>
2022-11-14 19:00:01 +0100

    #3 Kleine update

--- src/main/java/nu/educom/MI6/Login.java
+++ src/main/java/nu/educom/MI6/Login.java
@@ -15,49 +15,49 @@ public class Login {
     }
 
     public void checkLogin() {
-        JFrame frame = new JFrame("InputDialog Example #1");
-        boolean needCodeSentence = true;
-        int serviceNumber = 0;
-        String actualNumber = "";
+        JFrame frame                = new JFrame("Geen idee wat dit doet eigenlijk");
+        boolean needCodeSentence    = true;
+        int serviceNumber           = 0; // The variable that will be saved in blacklist & loggedIn
+        String showNumber           = ""; // The variable that will be shown to the user in communication
 
         while (true) {
+            // Wat doet dit eigenlijk?
             String jInput = JOptionPane.showInputDialog(frame, "Enter service number:");
             // If user clicks cancel, system quits.
             if (jInput == null) {System.exit(0);}
-
             // Otherwise checks the value
             try {
                 int userInput = Integer.parseInt(jInput);
                 serviceNumber = userInput;
-                actualNumber = String.format("%03d", serviceNumber);
+                showNumber = String.format("%03d", serviceNumber);
                 if (this.blacklist.contains(userInput)) {
-                    JOptionPane.showMessageDialog(null, String.format("You are still a failure %s", actualNumber));
+                    JOptionPane.showMessageDialog(frame, String.format("You are still a failure %s", showNumber));
                     needCodeSentence = false;
                     break;
                 } else if (this.loggedIn.contains(userInput)) {
-                    JOptionPane.showMessageDialog(null, String.format("You have already logged in %s", actualNumber));
+                    JOptionPane.showMessageDialog(frame, String.format("You are already logged in %s", showNumber));
                     needCodeSentence = false;
                     break;
                 } else if (0 < userInput && userInput < 957) {
                     break;
                 }
-                JOptionPane.showMessageDialog(null, "Which is false, please enter a number between 1-956");
+                JOptionPane.showMessageDialog(frame, "Which is false, please enter a number between 1-956");
             } catch (Exception e) {
-                JOptionPane.showMessageDialog(null,"Which is false, please enter a number");
+                JOptionPane.showMessageDialog(frame,"Which is false, please enter a number");
             }
         }
 
         if (needCodeSentence) {
-            String jInput = JOptionPane.showInputDialog(frame,  String.format("Please enter code-sentence %s", actualNumber));
+            String jInput = JOptionPane.showInputDialog(frame,  String.format("Please enter code-sentence %s", showNumber));
             // If user clicks cancel, system quits.
             if (jInput == null) {System.exit(0);}
             String userInput = jInput;
             if (userInput.equals(codeSentence)) {
-                JOptionPane.showMessageDialog(null, String.format("You have logged in successfully  %s", actualNumber));
+                JOptionPane.showMessageDialog(frame, String.format("You have logged in successfully  %s", showNumber));
                 loggedIn.add(serviceNumber);
 
             } else {
-                JOptionPane.showMessageDialog(null, String.format("ACCESS DENIED  %s", actualNumber));
+                JOptionPane.showMessageDialog(frame, String.format("ACCESS DENIED  %s", showNumber));
                 blacklist.add(serviceNumber);
             }
         }
